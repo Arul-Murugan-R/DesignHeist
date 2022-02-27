dfb66ad5c128570e42397f4c639fc2bf74a4f19d
Arul-Murugan-R <arulmuruganarjun14@gmail.com>
2022-02-27 16:21:59 +0530

    ott platform

--- index.js
+++ index.js
@@ -13,13 +13,35 @@ app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-	res.render("home",{data});
+	res.render("home",{
+		data:data,
+		path:req.originalUrl,
+		title:'Home Page'
+	});
 });
 
 app.get("/view", (req, res) => {
-	res.render("view", { data });
+	res.render("view", {
+		data:data,
+		path:req.originalUrl,
+		title:'View Page'
+	});
+});
+app.use("/movies", (req, res) => {
+	res.render("movies", {
+		data:data,
+		path:req.originalUrl,
+		title:'Movies Page'
+	});
+});
+app.use("/video", (req, res) => {
+	res.render("video", {
+		data:data,
+		path:req.originalUrl,
+		title:'Video Player'
+	});
 });
 
-app.listen(3000, () => {
-	console.log("Listening at 3000");
+app.listen(process.env.PORT||3000, () => {
+	console.log("Listening at http://localhost:3000");
 });
---
+++ public/javascripts/script.js
@@ -0,0 +1,96 @@
+let video = document.querySelector(".video");
+let juice = document.querySelector(".black-juice");
+let playPauseBtn = document.getElementById("pause-play");
+let forward = document.getElementById("forward");
+let backward = document.getElementById("backward");
+let sideOverlayBtn = document.getElementById("rounded-chevron-left");
+let sideTransition = document.getElementById("side-overlay");
+let isclicked = false;
+let optionsBtn = document.querySelector("#options-btn button");
+let threeDotsOverlay = document.getElementById("three-dots-overlay");
+let progressBar = document.getElementById("grey-bar");
+let fullScreen = document.getElementById("full-screen");
+function togglePlayPause() {
+    if (video.paused) {
+        playPauseBtn.classList.remove("fa-play");
+        playPauseBtn.classList.add("fa-pause");
+
+        video.play();
+    } else {
+        playPauseBtn.classList.remove("fa-pause");
+        playPauseBtn.classList.add("fa-play");
+
+        video.pause();
+    }
+}
+
+//Adding play-pause functionality to the video
+playPauseBtn.addEventListener("click", togglePlayPause);
+//increasing the width for the bar as the video gets updated every-second
+video.addEventListener("timeupdate", function () {
+    let juicePosition = video.currentTime / video.duration;
+    juice.style.width = juicePosition * 100 + "%";
+    if (video.currentTime != video.duration) {
+        juice.classList.remove("black-juice-completed");
+    } else {
+        juice.classList.add("black-juice-completed");
+    }
+    if (video.currentTime === video.duration) {
+        playPauseBtn.classList.add("fa-play");
+    }
+
+    console.log(video.currentTime);
+});
+
+// adding forward +5 and backward -5 for the video
+forward.addEventListener("click", function () {
+    video.currentTime += 5;
+});
+backward.addEventListener("click", function () {
+    video.currentTime -= 5;
+});
+
+// side-overlay with a smooth transition on clicking the beautiful chevron buttons moving left and right on clicking respectively
+
+sideOverlayBtn.addEventListener("click", function () {
+    let width = sideTransition.offsetWidth;
+    width -= Math.floor(sideOverlayBtn.offsetWidth / 3);
+    console.log(width);
+    if (!isclicked) {
+        sideTransition.style.transform = "translateX(0)";
+        sideOverlayBtn.style.transform = `translateX(-${width + 5}px)`;
+        sideOverlayBtn.innerHTML =
+            "<i class='fa-solid fa-circle-chevron-right fa-3x'></i>";
+        isclicked = true;
+    } else {
+        sideTransition.style.transform = "translateX(100%)";
+        sideOverlayBtn.style.transform = `translateX(25%)`;
+        sideOverlayBtn.innerHTML =
+            "<i class='fa-solid fa-circle-chevron-left fa-3x'></i>";
+
+        isclicked = false;
+    }
+});
+
+// increasingthe  width of the juice to the click on the offsetx coordinate
+progressBar.addEventListener("click", (event) => {
+    let videoDuration = video.duration;
+    let progressWidthValue = progressBar.clientWidth;
+    let clickOffsetX = event.offsetX;
+
+    video.currentTime = (clickOffsetX / progressWidthValue) * videoDuration;
+    juice.style.width = `${(clickOffsetX / progressWidthValue) * 100}%`;
+});
+//
+// function openFullscreen() {
+//     if (video.requestFullscreen) {
+//         video.requestFullscreen();
+//     } else if (video.webkitRequestFullscreen) {
+//         /* Safari */
+//         video.webkitRequestFullscreen();
+//     } else if (video.msRequestFullscreen) {
+//         /* IE11 */
+//         video.msRequestFullscreen();
+//     }
+// }
+// fullScreen.addEventListener("click", openFullscreen);
